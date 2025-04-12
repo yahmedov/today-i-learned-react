2920fe30d2bbf2d13f4a449ba24137af948f08b5
yahmedov <13299471+yahmedov@users.noreply.github.com>
2025-04-12 21:57:58 +0200

    show "disputed" label for falsy facts

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ function isValidHttpUrl(string) {
 
 function NewFactForm({ setFacts, setShowForm }) {
   const [text, setText] = useState('');
-  const [source, setSource] = useState('http://example.com');
+  const [source, setSource] = useState('');
   const [category, setCategory] = useState('');
   const [isUploading, setIsUploading] = useState(false);
   const textLength = text.length;
@@ -214,6 +214,7 @@ function FactList({ facts, setFacts }) {
 
 function Fact({ fact, setFacts }) {
   const [isUpdating, setIsUpdating] = useState(false);
+  const isDisputed = (fact.votesInteresting + fact.votesMindblowing) < fact.votesFalse;
   async function handleVote(columnName) {
     setIsUpdating(true);
 
@@ -232,6 +233,7 @@ function Fact({ fact, setFacts }) {
   return (
     <li className='fact'>
       <p>
+        {isDisputed ? <span className='disputed'>[⛔️ DISPUTED]</span> : null}
         {fact.text}
         <a className='source' href={fact.source} target='_blank' rel='noreferrer'>(Source)</a>
       </p>
